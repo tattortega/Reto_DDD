5486b22166acc74fa05b8788e1409bd2da55516f
Ricardo Ortega <tattortega.28@gmail.com>
2022-07-01 13:25:31 -0500

    Agregando evento para AutoAgregado

---
+++ domain/src/main/java/co/com/sofkau/taller/jefedepatio/events/AutoAgregado.java
@@ -0,0 +1,32 @@
+package co.com.sofkau.taller.jefedepatio.events;
+
+import co.com.sofka.domain.generic.DomainEvent;
+import co.com.sofkau.taller.jefedepatio.values.AutoId;
+import co.com.sofkau.taller.jefedepatio.values.Marca;
+import co.com.sofkau.taller.jefedepatio.values.Placa;
+
+
+public class AutoAgregado extends DomainEvent {
+    private final AutoId autoId;
+    private final Marca marca;
+    private final Placa placa;
+
+    public AutoAgregado(AutoId autoId, Marca marca, Placa placa) {
+        super("co.com.sofkau.taller.jefedepatio.AutoAgregado");
+        this.autoId = autoId;
+        this.marca = marca;
+        this.placa = placa;
+    }
+
+    public AutoId autoId() {
+        return autoId;
+    }
+
+    public Marca marca() {
+        return marca;
+    }
+
+    public Placa placa() {
+        return placa;
+    }
+}
