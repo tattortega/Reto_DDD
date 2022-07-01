2d8f320854086975ef501f9b9c5c1a74e35a6f09
Ricardo Ortega <tattortega.28@gmail.com>
2022-07-01 13:39:55 -0500

    Agregando comando para FinalizarReparacion

---
+++ domain/src/main/java/co/com/sofkau/taller/mecanico/commands/FinalizarReparacion.java
@@ -0,0 +1,69 @@
+package co.com.sofkau.taller.mecanico.commands;
+
+import co.com.sofka.domain.generic.Command;
+import co.com.sofkau.taller.jefedepatio.values.RegistroId;
+import co.com.sofkau.taller.mecanico.values.InspeccionId;
+import co.com.sofkau.taller.mecanico.values.MecanicoId;
+import co.com.sofkau.taller.mecanico.values.ReparacionId;
+import co.com.sofkau.taller.valuesgeneric.Estado;
+import co.com.sofkau.taller.valuesgeneric.Observacion;
+import co.com.sofkau.taller.valuesgeneric.TipoDeTrabajo;
+import co.com.sofkau.taller.vendedor.values.VendedorId;
+
+
+public class FinalizarReparacion extends Command {
+    private final VendedorId vendedorId;
+    private final MecanicoId mecanicoId;
+    private final RegistroId registroId;
+    private final ReparacionId reparacionId;
+    private final InspeccionId inspeccionId;
+    private final TipoDeTrabajo tipoDeTrabajo;
+    private final Estado estado;
+    private final Observacion observacion;
+
+    public FinalizarReparacion(VendedorId vendedorId, MecanicoId mecanicoId, RegistroId registroId,
+                               ReparacionId reparacionId, InspeccionId inspeccionId,
+                               TipoDeTrabajo tipoDeTrabajo, Estado estado,
+                               Observacion observacion) {
+        this.vendedorId = vendedorId;
+        this.mecanicoId = mecanicoId;
+        this.registroId = registroId;
+        this.reparacionId = reparacionId;
+        this.tipoDeTrabajo = tipoDeTrabajo;
+        this.estado = estado;
+        this.observacion = observacion;
+        this.inspeccionId = inspeccionId;
+    }
+
+    public TipoDeTrabajo tipoDeTrabajo() {
+        return tipoDeTrabajo;
+    }
+
+    public VendedorId vendedorId() {
+        return vendedorId;
+    }
+
+    public MecanicoId mecanicoId() {
+        return mecanicoId;
+    }
+
+    public RegistroId registroId() {
+        return registroId;
+    }
+
+    public ReparacionId reparacionId() {
+        return reparacionId;
+    }
+
+    public Estado estado() {
+        return estado;
+    }
+
+    public Observacion observacion() {
+        return observacion;
+    }
+
+    public InspeccionId inspeccionId() {
+        return inspeccionId;
+    }
+}
