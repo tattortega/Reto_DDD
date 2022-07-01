617ea48cc67afe3d02e3fdf091736965b3a3d2b8
Ricardo Ortega <tattortega.28@gmail.com>
2022-07-01 13:23:35 -0500

    Agregando value object para Placa

---
+++ domain/src/main/java/co/com/sofkau/taller/jefedepatio/values/Placa.java
@@ -0,0 +1,35 @@
+package co.com.sofkau.taller.jefedepatio.values;
+
+import co.com.sofka.domain.generic.ValueObject;
+
+import java.util.Objects;
+
+
+public class Placa implements ValueObject<String> {
+    private final String value;
+
+    public Placa(String value) {
+        this.value = Objects.requireNonNull(value);
+        if (this.value.isBlank()) {
+            throw new IllegalArgumentException("La placa no puede estar vacia");
+        }
+    }
+
+    @Override
+    public String value() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Placa)) return false;
+        Placa placa = (Placa) o;
+        return Objects.equals(value, placa.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
