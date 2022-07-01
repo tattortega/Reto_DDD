01e92c4e2b9d0ead5ee6df64aeb71eacc803f9f5
Ricardo Ortega <tattortega.28@gmail.com>
2022-07-01 13:25:42 -0500

    Agregando evento para ClienteAgregado

---
+++ domain/src/main/java/co/com/sofkau/taller/jefedepatio/events/ClienteAgregado.java
@@ -0,0 +1,46 @@
+package co.com.sofkau.taller.jefedepatio.events;
+
+import co.com.sofka.domain.generic.DomainEvent;
+import co.com.sofkau.taller.jefedepatio.values.ClienteId;
+import co.com.sofkau.taller.jefedepatio.values.Documento;
+import co.com.sofkau.taller.valuesgeneric.Correo;
+import co.com.sofkau.taller.valuesgeneric.Nombre;
+import co.com.sofkau.taller.valuesgeneric.Telefono;
+
+
+public class ClienteAgregado extends DomainEvent {
+    private final ClienteId clienteId;
+    private final Documento documento;
+    private final Nombre nombre;
+    private final Telefono telefono;
+    private final Correo correo;
+
+    public ClienteAgregado(ClienteId clienteId, Documento documento, Nombre nombre, Telefono telefono, Correo correo) {
+        super("co.com.sofkau.taller.jefedepatio.ClienteAgregado");
+        this.clienteId = clienteId;
+        this.documento = documento;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.correo = correo;
+    }
+
+    public ClienteId clienteId() {
+        return clienteId;
+    }
+
+    public Documento documento() {
+        return documento;
+    }
+
+    public Nombre nombre() {
+        return nombre;
+    }
+
+    public Telefono telefono() {
+        return telefono;
+    }
+
+    public Correo correo() {
+        return correo;
+    }
+}
