2f21e2168e26b2c7b9d1036473952338e735e02e
Ricardo Ortega <tattortega.28@gmail.com>
2022-07-01 13:16:59 -0500

    Agregando value object generico para Correo

---
+++ domain/src/main/java/co/com/sofkau/taller/valuesgeneric/Correo.java
@@ -0,0 +1,32 @@
+package co.com.sofkau.taller.valuesgeneric;
+
+import co.com.sofka.domain.generic.ValueObject;
+
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class Correo implements ValueObject<String> {
+    private final String value;
+
+    public Correo(String value) {
+        this.value = Objects.requireNonNull(value);
+        if (this.value.isBlank()) {
+            throw new IllegalArgumentException("El correo no puede estar vacio");
+        }
+        Pattern pattern = Pattern
+                .compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                        + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+        Matcher mather = pattern.matcher(value);
+
+        if (!mather.find()) {
+            throw new IllegalArgumentException("El email ingresado es inválido.");
+        }
+    }
+
+    @Override
+    public String value() {
+        return value;
+    }
+}
