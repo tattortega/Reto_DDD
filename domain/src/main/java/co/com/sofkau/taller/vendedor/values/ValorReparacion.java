d9df64a31a4804cfa40e8981dd3f248e921f0d49
Ricardo Ortega <tattortega.28@gmail.com>
2022-07-01 13:52:38 -0500

    Agregando object value para ValorReparacion

---
+++ domain/src/main/java/co/com/sofkau/taller/vendedor/values/ValorReparacion.java
@@ -0,0 +1,34 @@
+package co.com.sofkau.taller.vendedor.values;
+
+import co.com.sofka.domain.generic.ValueObject;
+
+import java.util.Objects;
+
+public class ValorReparacion implements ValueObject<Integer> {
+    private final Integer value;
+
+    public ValorReparacion(Integer value) {
+        this.value = Objects.requireNonNull(value);
+        if (this.value < 0) {
+            throw new IllegalArgumentException("El valor de la reparacion no puede ser menor que cero");
+        }
+    }
+
+    @Override
+    public Integer value() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ValorReparacion)) return false;
+        ValorReparacion that = (ValorReparacion) o;
+        return Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
