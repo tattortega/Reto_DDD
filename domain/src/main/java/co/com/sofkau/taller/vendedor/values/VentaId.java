61df071f6819e008f94a8b8106d2a02b43478d6f
Ricardo Ortega <tattortega.28@gmail.com>
2022-07-01 13:50:42 -0500

    Agregando object value tipo Identity para Venta

---
+++ domain/src/main/java/co/com/sofkau/taller/vendedor/values/VentaId.java
@@ -0,0 +1,16 @@
+package co.com.sofkau.taller.vendedor.values;
+
+import co.com.sofka.domain.generic.Identity;
+
+public class VentaId extends Identity {
+    public VentaId(String id) {
+        super(id);
+    }
+
+    public VentaId() {
+    }
+
+    public static VentaId of(String id) {
+        return new VentaId(id);
+    }
+}
