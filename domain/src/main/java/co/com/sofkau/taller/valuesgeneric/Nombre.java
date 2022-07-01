803c82bd7010baaff132657555a9482e0b1486bb
Ricardo Ortega <tattortega.28@gmail.com>
2022-07-01 13:17:22 -0500

    Agregando value object generico para Nombre

---
+++ domain/src/main/java/co/com/sofkau/taller/valuesgeneric/Nombre.java
@@ -0,0 +1,35 @@
+package co.com.sofkau.taller.valuesgeneric;
+
+import co.com.sofka.domain.generic.ValueObject;
+
+import java.util.Objects;
+
+
+public class Nombre implements ValueObject<String> {
+    private final String value;
+
+    public Nombre(String value) {
+        this.value = Objects.requireNonNull(value);
+        if (this.value.isBlank()) {
+            throw new IllegalArgumentException("El nombre no puede estar vacio");
+        }
+    }
+
+    @Override
+    public String value() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Nombre nombre = (Nombre) o;
+        return Objects.equals(value, nombre.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
