e860ffd5fc30c3c0cccf323c6124ff89bef6a9b4
Ricardo Ortega <tattortega.28@gmail.com>
2022-07-01 17:13:07 -0500

    Agregando test para el caso de uso FinalizarReparacion

---
+++ business/src/test/java/co/com/sofkau/taller/mecanico/FinalizarReparacionUseCaseTest.java
@@ -0,0 +1,92 @@
+package co.com.sofkau.taller.mecanico;
+
+import co.com.sofka.business.generic.UseCaseHandler;
+import co.com.sofka.business.repository.DomainEventRepository;
+import co.com.sofka.business.support.RequestCommand;
+import co.com.sofka.domain.generic.DomainEvent;
+import co.com.sofkau.taller.jefedepatio.values.RegistroId;
+import co.com.sofkau.taller.mecanico.commands.FinalizarReparacion;
+import co.com.sofkau.taller.mecanico.events.InspeccionAgregada;
+import co.com.sofkau.taller.mecanico.events.MecanicoAsignado;
+import co.com.sofkau.taller.mecanico.events.ReparacionAgregada;
+import co.com.sofkau.taller.mecanico.events.ReparacionFinalizada;
+import co.com.sofkau.taller.mecanico.values.*;
+import co.com.sofkau.taller.valuesgeneric.*;
+import co.com.sofkau.taller.vendedor.values.VendedorId;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class FinalizarReparacionUseCaseTest {
+
+    @InjectMocks
+    FinalizarReparacionUseCase finalizarReparacionUseCase;
+
+    @Mock
+    private DomainEventRepository repository;
+
+    @Test
+    void finalizarReparacion() {
+        var vendedorId = VendedorId.of("03");
+        var mecanidoId = MecanicoId.of("054");
+        var registroId = RegistroId.of("452");
+        var reparacionId = ReparacionId.of("4522");
+        var inspeccionId = InspeccionId.of("33253");
+        var tipoDeTrabajo = new TipoDeTrabajo(TipoDeTrabajo.Tipos.REPARACION);
+        var estados = new Estado(Estado.Estados.EMPEZADO);
+        var observacion = new Observacion("Se debe cambiar la bateria por otra");
+        var command = new FinalizarReparacion(vendedorId, mecanidoId, registroId, reparacionId, inspeccionId,
+                tipoDeTrabajo, estados, observacion);
+
+        when(repository.getEventsBy("054")).thenReturn(history());
+        finalizarReparacionUseCase.addRepository(repository);
+
+        var events = UseCaseHandler.getInstance()
+                .setIdentifyExecutor(command.mecanicoId().value())
+                .syncExecutor(finalizarReparacionUseCase, new RequestCommand<>(command))
+                .orElseThrow()
+                .getDomainEvents();
+
+        var event = (ReparacionFinalizada) events.get(0);
+        Assertions.assertEquals(Estado.Estados.FINALIZADO, event.estado().value());
+    }
+
+    private List<DomainEvent> history() {
+        var vendedorId = VendedorId.of("03");
+        var registroId = RegistroId.of("452");
+        var nombre = new Nombre("Cristian");
+        var telefono = new Telefono("3125632563");
+        var correo = new Correo("cristian@chevrocentro.com");
+        var tipoDeTrabajo = new TipoDeTrabajo(TipoDeTrabajo.Tipos.REPARACION);
+
+        var event = new MecanicoAsignado(vendedorId, registroId, nombre, telefono, correo, tipoDeTrabajo);
+
+        var inspeccionId = InspeccionId.of("33253");
+        var diagnostico = new Diagnostico("La bateria hizo corto circuito");
+        Set<ListaRepuestos> listaRepuestos = new HashSet<>();
+        var repuesto1 = new ListaRepuestos("Bateria Mac Silver 36IST750MC");
+        listaRepuestos.add(repuesto1);
+
+        var event2 = new InspeccionAgregada(inspeccionId, diagnostico, listaRepuestos);
+
+        var reparacionId = ReparacionId.of("4522");
+        var estados = new Estado(Estado.Estados.EMPEZADO);
+        var observacion = new Observacion("Se debe cambiar la bateria por otra");
+
+        var event3 = new ReparacionAgregada(reparacionId, tipoDeTrabajo, estados, observacion);
+
+        event.setAggregateRootId("054");
+        return List.of(event, event2, event3);
+    }
+
+}
